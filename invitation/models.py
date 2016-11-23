ee1c551f31ec557b7654027fd0cb88cf141312d9
Gabriel Augendre <gabriel@augendre.info>
2016-11-23 17:41:57 +0100

    Fix capital first letter in models and fields

--- invitation/models.py
+++ invitation/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class Guest(models.Model):
     class Meta:
-        verbose_name = 'Invité'
+        verbose_name = 'invité'
 
-    name = models.CharField(max_length=255, verbose_name='Nom')
+    name = models.CharField(max_length=255, verbose_name='nom')
     email = models.EmailField()
-    invited_by = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Invité par')
-    type = models.ForeignKey('Type', null=False, blank=False, verbose_name='Type')
+    invited_by = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='invité par')
+    type = models.ForeignKey('Type', null=False, blank=False, verbose_name='type')
     created_at = models.DateTimeField(auto_now_add=True)
     max_seats = models.IntegerField(default=1)
 
@@ -19,9 +19,9 @@ class Guest(models.Model):
 
 class Type(models.Model):
     class Meta:
-        verbose_name = 'Type'
+        verbose_name = 'type'
 
-    name = models.CharField(max_length=255, verbose_name='Nom')
+    name = models.CharField(max_length=255, verbose_name='nom')
 
     def __str__(self):
         return self.name
@@ -29,8 +29,8 @@ class Type(models.Model):
 
 class Order(models.Model):
     class Meta:
-        verbose_name = 'Commande'
+        verbose_name = 'commande'
 
     yurplan_id = models.CharField(max_length=100, verbose_name='ID Yurplan')
-    seats_count = models.IntegerField(verbose_name='Nombre de places')
+    seats_count = models.IntegerField(verbose_name='nombre de places')
     guest = models.ForeignKey('Guest', null=False, blank=False)
