4eaa146b1dc47f2b076c1cf01d8b752aca42a69a
Philippe Vienne <philippegeek@gmail.com>
2016-11-24 12:02:07 +0100

    Working on iFrame mangement

--- invitation/static/invitation/shop.js
+++ invitation/static/invitation/shop.js
@@ -1,4 +1,15 @@
 /**
  * Created by pvienne on 23/11/2016.
  */
-$("#iFrame").contents().find("#someDiv").removeClass("hidden");
+
+$(function(){
+    var $shop = $("#yurplan-iframe").contents();
+
+    var loop = function(){
+        if($shop.find('.password_event')){
+            $shop.find('input[name=password]').val('bdeinsa').parent().submit();
+        }
+    };
+
+    setInterval(loop, 100);
+});
