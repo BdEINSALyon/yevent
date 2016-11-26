571df3df33443715ce03589a2e546af8f523c616
Philippe Vienne <philippegeek@gmail.com>
2016-11-26 16:21:26 +0100

    Send email

--- invitation/admin.py
+++ invitation/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
+from invitation import email
 from .models import Guest, Order, Type
 
 
@@ -9,7 +10,15 @@ class GuestAdmin(ImportExportModelAdmin):
     actions = ['send_email']
 
     def send_email(self, request, queryset):
-        pass
+        count = 0
+        for guest in queryset.all():
+            # noinspection PyBroadException
+            try:
+                email.send_email(guest)
+                count += 1
+            except:
+                pass
+        self.message_user(request, "{} email(s) envoyé à {} personne(s)".format(count, queryset.count()))
 
     send_email.short_description = "Envoyer le mail d'invitation"
 
